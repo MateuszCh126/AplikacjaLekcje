ad283e6322d9ca0ae1b006c8ac93c29c8db3ebfc
Mateusz <121506855+MateuszCh126@users.noreply.github.com>
2023-05-16 14:21:19 +0200

    03

--- main/index.py
+++ main/index.py
@@ -4,7 +4,7 @@ import sqlite3
 import hashlib
 #lączenie z baza danych
 global conn 
-conn= sqlite3.connect('Uzytkownicy.db')
+conn= sqlite3.connect('Uzytkownicy.db',check_same_thread=False)
 
 # utworzenie tabeli uzytkownicy
 conn.execute("""
@@ -22,40 +22,53 @@ conn.commit()
 #strona wyswietlana 
 def main(page: ft.Page):
     page.title = "logowanie"
-    page.vertical_alignment = ft.MainAxisAlignment.START
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     #logowanie tego typu
     
     Zaloguj=ft.Text(value="Zaloguj sie1", color="grey",text_align="CENTER",font_family='ARIAL')
-    Nazwa=ft.TextField(label="Nazwa: ",text_align="left",width=300, autofocus=True)
-    Hasło=ft.TextField(label="Hasło: ",text_align="left", autofocus=True,password=True)
+    txt_Nazwa=ft.TextField(label="Nazwa: ",text_align="left",width=300, autofocus=True)
+    txt_Hasło=ft.TextField(label="Hasło: ",text_align="left", autofocus=True,password=True)
     print('hej')
-    page.add(Zaloguj,Nazwa,Hasło)
+    page.add(Zaloguj,txt_Nazwa,txt_Hasło)
     def button_clicked(e):
+        if not txt_Nazwa.value:
+            txt_Nazwa.error_text = "Please enter your name"
+            page.update()
+        else:
+            name = txt_Nazwa.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {name}!"))
+        if not txt_Hasło.value:
+            txt_Hasło.error_text = "Please enter your name"
+            page.update()
+        else:
+            password = txt_Hasło.value
+            page.clean()
+            page.add(ft.Text(f"Hello, {password}!"))
+        def hash_password(password): 
+
+            return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        hashed_password = hash_password(password)
+        
         cursor = conn.cursor()
-        cursor.execute("SELECT imie , haslo FROM uzytkownicy WHERE imie ='{Nazwa}' AND haslo ='{Hasło}'")
+        cursor.execute(f"SELECT imie , haslo FROM uzytkownicy WHERE imie ='{name}' AND haslo ='{password}'")
         result= cursor.fetchone()
-        print('hej')
+        
         if result:
-            print('hej')
             page.add(ft.Text("Zalogowano!1"))
             Nazwa, Hasło = result
-            page.add(result)
             if cursor.execute("SELECT czy_admin FROM uzytkownicy WHERE czy_admin=1",):
-                print('hej')
                 page.add(ft.Text("Użytkownik jest administratorem.!"))
                 page.add(ft.Text("Zalogowano!"))
             else:
-                print('hej')
                 page.add(ft.Text("Użytkownik NIE jest administratorem.!"))
                 page.add(ft.Text("Zalogowano! ALE NIE ADMIN"))
         else:
-            print('hej')
             page.add(ft.Text("NIE Zalogowano! ZLE DANE"))
 
     page.add(ft.ElevatedButton(text="Zaloguj się", on_click=button_clicked,))
 #zabezpieczenie hasla bo to wiesz pro ludzie robia TO DO REJESTRACJI BTW
-def hash_password(password): 
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
 ft.app(target=main)
-conn.close()  
+ 
--- main/main.py
+++ main/main.py
@@ -37,12 +37,14 @@ def main(page: ft.Page):
         ft.Column(controls=[
         ft.Text(ref=Zaloguj, value="Zaloguj sie", color="grey",text_align="CENTER",font_family='ARIAL'),
         ft.TextField(ref=Nazwa, label="Nazwa: ",text_align="left",width=300, autofocus=True),
-        ft.TextField(ref=Hasło, label="Hasło: ",text_align="left", autofocus=True,password=True)
+        ft.TextField(ref=Hasło, label="Hasło: ",text_align="left",width=300 ,autofocus=True,password=True)
     ],alignment=ft.MainAxisAlignment.START))
+    
+
     def button_clicked(e):
         conn= sqlite3.connect('Uzytkownicy.db',check_same_thread=False)
         cursor = conn.cursor()
-        cursor.execute("SELECT imie , haslo FROM uzytkownicy WHERE imie ='A' AND haslo ='A'")
+        cursor.execute("SELECT imie , haslo FROM uzytkownicy WHERE imie ='{Nazwaa}' AND haslo ='{Hasłoo}'")
         result= cursor.fetchone()
         print(result)
         print(cursor)
@@ -50,17 +52,17 @@ def main(page: ft.Page):
             page.add(ft.Text("Zalogowano!1"))
             Nazwa, Hasło = result
             if cursor.execute("SELECT czy_admin FROM uzytkownicy WHERE czy_admin=1",):
-                print('hej')
+                print('hej0')
                 page.add(ft.Text("Użytkownik jest administratorem.!"))
                 page.add(ft.Text("Zalogowano!"))
                 
             else:
-                print('hej')
+                print('hej1')
                 page.add(ft.Text("Użytkownik NIE jest administratorem.!"))
                 page.add(ft.Text("Zalogowano! ALE NIE ADMIN"))
                 
         else:
-            print('hej')
+            print('hej2')
             page.add(ft.Text("NIE Zalogowano! ZLE DANE")) 
     page.add(ft.ElevatedButton(text="Zaloguj się", on_click=button_clicked,))
 
